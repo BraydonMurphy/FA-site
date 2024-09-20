13cfe66718bd4662caf3c3043947fbe116fd9ad4
BJ Murphy <178152896+BraydonMurphy@users.noreply.github.com>
2024-09-20 06:23:44 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var topButton = document.getElementById("topBtn");
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+};
+
+function topFunction() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
